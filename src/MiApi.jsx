90af0dfbe43a8ac75718b1503dde18c095abd9c0
Jonas <jonathan.brioneslavados@cencosud.cl>
2022-09-26 18:26:06 -0300

    modificaciones en los archivos

--- src/MiApi.jsx
+++ src/MiApi.jsx
@@ -7,19 +7,24 @@ const PRIVATE_KEY = '9e45f2e453825ac75fee5751d9e44ce965715877'; // youur private
 export const MiApi = () => {
   const [heroes, setHeroes] = useState([])
   const [ordenar, setOrdenar] = useState('name')
+  const [nombre, setNombre] = useState("")
 
   const handleOrdenar = (e) =>{
       setOrdenar(e.target.value);
   }
 
-  const buscarNombre = async () => {
+  const handleInputNombre = (e) => {
+    setNombre(e.target.value);
+  }
+  const buscarNombre = async (e) => {
+    e.preventDefault();
     const ts = Number(new Date());
     const hash = md5.create();
     hash.update(ts + PRIVATE_KEY + PUBLIC_KEY);
     
     try {
       const response = await fetch(
-        `https://gateway.marvel.com/v1/public/characters?ts=${ts}&orderBy=${ordenar}&limit=20&query=Batman&apikey=${PUBLIC_KEY}&hash=${hash}`);
+        `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${nombre}&ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`);
        const info = await response.json();
        setHeroes( info.data.results );
     } catch (error) {
@@ -44,7 +49,6 @@ export const MiApi = () => {
 
   useEffect(() => {
     consultarsuperheroes();
-    
   }, [ordenar])
   
   console.log(heroes);
@@ -54,24 +58,27 @@ export const MiApi = () => {
           <div className= "container-fluid" >
 
             <div className='container'>
-              <form className="form-inline">
-              <div class="row">
-                <div class="col">
+              <div className="row">
+                <div className="col">
                 
+              <form className="form-inline" onSubmit={buscarNombre}>
                     
-                  <div class="input-group mb-3">
-                  <input type="text" class="form-control" placeholder="Buscar por nombre" aria-label="Buscar por nombre" aria-describedby="button-addon2" />
-                  <div class="input-group-append">
-                    <button class="btn btn-outline-primary" type="button" id="button-addon2">Buscar</button>
+                  <div className="input-group mb-3">
+                  <input type="text" className="form-control" onChange={handleInputNombre} value={nombre} placeholder="Buscar por nombre" aria-label="Buscar por nombre" aria-describedby="button-addon2"/>
+                  <div className="input-group-append">
+                    <button className="btn btn-outline-primary" type="submmit" id="button-addon2">
+                      Buscar
+                    </button>
                   </div>
                 </div>
+              </form>
                     
                 
                 </div>
 
-                <div class="col-2">
+                <div className="col-2">
                
-                    <select class="form-control" id="exampleFormControlSelect1"
+                    <select className="form-control" id="exampleFormControlSelect1"
                       value={ordenar}
                       onChange={handleOrdenar}
                     >
@@ -81,8 +88,7 @@ export const MiApi = () => {
                  
                 </div>
               </div>
-
-              </form>
+              <a href="/" >Limpiar</a>
             </div>
             
             <div className= "container mt-4">
@@ -91,7 +97,7 @@ export const MiApi = () => {
                   ?
                     <>
                     { heroes.map( (heroe) => 
-                      <div className="card m-2 shadow rounded">
+                      <div className="card m-2 shadow rounded" key={heroe.id}>
                       <div className="card-body">
                         <div className="container">
                           <div className="row">
